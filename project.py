80fc0e3148d85721b6715076cded6d2f5a476c4c
saeed <engsaciidessa@gmail.com>
2026-02-02 07:47:58 +0300

    type script practice code

---
+++ project.py
@@ -0,0 +1,10 @@
+#type scripting = a process to covert a variable to from one data type to another
+#      str() , float(), int() , boolen()
+
+
+gpa= 30
+arpg = 30.5
+is_student = True 
+name ="student"
+
+print(type(gpa),type(arpg),type(is_student),type (name))
